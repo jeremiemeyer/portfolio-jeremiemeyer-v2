d4b8369389915ff77bc38183885fc33419ea8fed
Jérémie Meyer <jeremie.meyer357@gmail.com>
2023-11-27 10:10:01 +0100

    fix

--- src/lib/api/wakatime.ts
+++ src/lib/api/wakatime.ts
@@ -10,7 +10,7 @@ const api = wretch("https://wakatime.com", { cache: "no-store" })
 export const getCodingStats = async () => {
   return await api
     .headers({
-      Authorization: `Basic ${Buffer.from("waka_6683c4bd-e206-4936-b99f-fe1952a46595").toString(
+      Authorization: `Basic ${Buffer.from("waka_9c294d1c-a053-4997-bda7-16d4e2615114").toString(
         "base64"
       )}`,
     })
