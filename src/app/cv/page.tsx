f9960ac1766e3ae3b0d78774c931cf92594e4261
Jérémie Meyer <jeremie.meyer357@gmail.com>
2023-12-06 14:45:47 +0100

    enhanced style

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -1,4 +1,3 @@
-import HeadingText from "@/components/common/heading-text"
 import Cv from "@/components/pages/cv/cv"
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
@@ -9,25 +8,19 @@ export const metadata = {
   description: "Mon CV",
 }
 
-export function ButtonWithIcon() {
-  return (
-    <Link
-      href={`/static/cv_jeremie_meyer.pdf`}
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <Button>
-        <Download className="mr-2 h-4 w-4" /> Télécharger le PDF
-      </Button>
-    </Link>
-  )
-}
-
 export default function page() {
   return (
     <main className="fade-in container py-4 sm:py-8 flex flex-col items-center mb-12 gap-4">
       <Cv />
-      <ButtonWithIcon />
+      <Link
+        href={`/static/cv_jeremie_meyer.pdf`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Button>
+          <Download className="mr-2 h-4 w-4" /> Télécharger le PDF
+        </Button>
+      </Link>
     </main>
   )
 }
